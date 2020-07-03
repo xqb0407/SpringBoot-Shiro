5c0ec9c57d0dd7e7cd529335944c42b864c99c59
34675 <34675628@qq.com>
2020-07-03 17:03:59 +0800

    创建工程

---
+++ src/test/java/xyz/herther/SpringbootShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.herther;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
